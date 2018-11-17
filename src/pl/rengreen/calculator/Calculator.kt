63cb8e72b9923fc7a100d94ecd9de714213712b1
rengreen <r.m.gardzinska@gmail.com>
2018-11-17 19:52:19 +0100

    adding main menu

--- src/pl/rengreen/calculator/Calculator.kt
+++ src/pl/rengreen/calculator/Calculator.kt
@@ -1,2 +1,20 @@
 package pl.rengreen.calculator
 
+fun main(args: Array<String>) {
+    fun displayOptions() {
+        println("""
+        =============================================
+        Wciśnij znak lub literę aby wybrać działanie:
+        + dodawanie
+        - odejmowanie
+        * mnozenie
+        / dzielenie
+        q wyjscie z programu
+        """.trimIndent())
+    }
+
+    println("To jest kalkulator")
+    do {
+        displayOptions()
+    } while (false)
+}
