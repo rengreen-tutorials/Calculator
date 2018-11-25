8187765f2f7f1f2e41ec7f87baff365d46ce8880
rengreen <r.m.gardzinska@gmail.com>
2018-11-25 17:26:15 +0100

    adding enum class with operation definitions

--- src/pl/rengreen/calculator/Calculator.kt
+++ src/pl/rengreen/calculator/Calculator.kt
@@ -1,5 +1,20 @@
 package pl.rengreen.calculator
 
+enum class Operation {
+    ADD, SUBTRACT, MULTIPLY, DIVIDE, EXIT, WRONG_OPERATOR;
+    fun isMathOperation() = this != EXIT && this != WRONG_OPERATOR
+    companion object {
+        fun fromString(value: String?) = when (value) {
+            "+" -> ADD
+            "-" -> SUBTRACT
+            "*" -> MULTIPLY
+            "/" -> DIVIDE
+            "q" -> EXIT
+            else -> WRONG_OPERATOR
+        }
+    }
+}
+
 fun main(args: Array<String>) {
     fun displayOptions() {
         println("""
