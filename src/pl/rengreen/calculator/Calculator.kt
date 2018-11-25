f9467eabda15df44b9bf3d82bf30ae1b6c807be8
rengreen <r.m.gardzinska@gmail.com>
2018-11-25 17:47:12 +0100

    adding input and logic for 4 mathematical operations

--- src/pl/rengreen/calculator/Calculator.kt
+++ src/pl/rengreen/calculator/Calculator.kt
@@ -3,6 +3,7 @@ package pl.rengreen.calculator
 enum class Operation {
     ADD, SUBTRACT, MULTIPLY, DIVIDE, EXIT, WRONG_OPERATOR;
     fun isMathOperation() = this != EXIT && this != WRONG_OPERATOR
+
     companion object {
         fun fromString(value: String?) = when (value) {
             "+" -> ADD
@@ -16,16 +17,37 @@ enum class Operation {
 }
 
 fun main(args: Array<String>) {
-    fun displayOptions() {
-        println("""
+    fun displayOptions() = println("""
         =============================================
         Wciśnij znak lub literę aby wybrać działanie:
         + dodawanie
         - odejmowanie
-        * mnozenie
+        * mnożenie
         / dzielenie
-        q wyjscie z programu
+        q wyjście z programu
         """.trimIndent())
+
+    fun readArguments() = readLine()!!
+        .split(" ")
+        .mapNotNull {
+            try {
+                it.toLong()
+            } catch (nfe: NumberFormatException) {
+                null
+            }
+        }
+
+    fun List<Long>.areArgumentsValid() = this.size == 2
+    fun doMath(operation: Operation, arguments: List<Long>) {
+        val (a, b) = arguments
+        val result: Number = when (operation) {
+            Operation.ADD -> a + b
+            Operation.SUBTRACT -> a - b
+            Operation.MULTIPLY -> a * b
+            Operation.DIVIDE -> a / b.toDouble()
+            else -> 0
+        }
+        println("Wynik: ${result.toDouble()}\n")
     }
 
     println("To jest kalkulator")
@@ -34,7 +56,13 @@ fun main(args: Array<String>) {
         val operation = Operation.fromString(readLine())
         when {
             operation.isMathOperation() -> {
-            // TODO
+                println("Podaj dwie liczby całkowite oddzielone spacją")
+                val arguments = readArguments()
+                if (arguments.areArgumentsValid()) doMath(operation, arguments) else println("""
+                    Nie podałeś dwóch liczb całkowitych oddzielonych spacją.
+                    Spróbuj ponownie:
+
+                    """.trimIndent())
             }
             operation == Operation.EXIT -> System.exit(0)
             else -> println("Wybrałeś znak, którego nie ma na liście")
